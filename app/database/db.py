a266713852f3a85a3f5d2b4ce5ef37f29d76e640
sjbl69 <selma.jbl92@gmail.com>
2026-03-11 16:42:21 +0100

    Fix db import create_engine

--- app/database/db.py
+++ app/database/db.py
@@ -1,3 +1,6 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 import os
 
 DATABASE_URL = os.getenv(
@@ -8,4 +11,10 @@ DATABASE_URL = os.getenv(
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )

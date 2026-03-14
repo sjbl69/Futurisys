1e46d8280b209fbd815f5ffc03570885d40aa7ee
sjbl69 <selma.jbl92@gmail.com>
2026-03-14 23:40:49 +0100

    update create_tables and database structure

--- create_tables.py
+++ create_tables.py
@@ -1,9 +1,10 @@
 from app.database.db import engine, Base
-from app.database.models import Prediction
+from app.database import models
 
+def create_tables():
+    print("Création des tables...")
+    Base.metadata.create_all(bind=engine)
+    print("Tables créées avec succès")
 
-print("Création des tables...")
-
-Base.metadata.create_all(bind=engine)
-
-print("Tables créées avec succès")
+if __name__ == "__main__":
+    create_tables()
